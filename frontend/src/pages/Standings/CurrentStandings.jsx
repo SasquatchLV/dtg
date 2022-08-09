94d541c28c0f2aac4bb1552905a826895d4d6b82
Haralds Treilons <100586228+GARISoo@users.noreply.github.com>
2022-08-09 16:46:13 +0300

    Seasons can be added

--- frontend/src/pages/Standings/CurrentStandings.jsx
+++ frontend/src/pages/Standings/CurrentStandings.jsx
@@ -7,6 +7,7 @@ import { errorToast } from '../../utils/toast'
 const CurrentStandings = () => {
   const [groupA, setGroupA] = useState([])
   const [groupB, setGroupB] = useState([])
+  const [activeSeason, setActiveSeason] = useState(false)
   const { user } = useAuthContext()
 
   const getAllTeams = async () => {
@@ -33,64 +34,69 @@ const CurrentStandings = () => {
   }, [user])
 
   return (
-    <div className={styles.container}>
-      <div className={styles.teamWrapper}>
-        <h4>GROUP A</h4>
-        <div className={styles.teamData}>
-          <span>Country</span>
-          <span>Won</span>
-          <span>Lost</span>
-          <span>WO</span>
-          <span>LO</span>
-          <span>GP</span>
-          <span>Points</span>
+    groupA.length ? (
+
+      <div className={styles.container}>
+        <div className={styles.teamWrapper}>
+          <h4>GROUP A</h4>
+          <div className={styles.teamData}>
+            <span>Country</span>
+            <span>Won</span>
+            <span>Lost</span>
+            <span>WO</span>
+            <span>LO</span>
+            <span>GP</span>
+            <span>Points</span>
+          </div>
+          {groupA.map(({
+            _id, country, flag, gamesWon, gamesLost, gamesWO, gamesLO, points,
+          }) => (
+            <TeamsCard
+              key={_id}
+              _id={_id}
+              country={country}
+              flag={flag}
+              gamesWon={gamesWon}
+              gamesLost={gamesLost}
+              gamesWO={gamesWO}
+              gamesLO={gamesLO}
+              points={points}
+              deletable={false}
+            />
+          ))}
         </div>
-        {groupA.map(({
-          _id, country, flag, gamesWon, gamesLost, gamesWO, gamesLO, points,
-        }) => (
-          <TeamsCard
-            key={_id}
-            _id={_id}
-            country={country}
-            flag={flag}
-            gamesWon={gamesWon}
-            gamesLost={gamesLost}
-            gamesWO={gamesWO}
-            gamesLO={gamesLO}
-            points={points}
-            deletable={false}
-          />
-        ))}
-      </div>
-      <div className={styles.teamWrapper}>
-        <h4>GROUP B</h4>
-        <div className={styles.teamData}>
-          <span>Country</span>
-          <span>Won</span>
-          <span>Lost</span>
-          <span>WO</span>
-          <span>LO</span>
-          <span>GP</span>
-          <span>Points</span>
+        <div className={styles.teamWrapper}>
+          <h4>GROUP B</h4>
+          <div className={styles.teamData}>
+            <span>Country</span>
+            <span>Won</span>
+            <span>Lost</span>
+            <span>WO</span>
+            <span>LO</span>
+            <span>GP</span>
+            <span>Points</span>
+          </div>
+          {groupB.map(({
+            _id, country, flag, gamesWon, gamesLost, gamesWO, gamesLO, points,
+          }) => (
+            <TeamsCard
+              key={_id}
+              _id={_id}
+              country={country}
+              flag={flag}
+              gamesWon={gamesWon}
+              gamesLost={gamesLost}
+              gamesWO={gamesWO}
+              gamesLO={gamesLO}
+              points={points}
+              deletable={false}
+            />
+          ))}
         </div>
-        {groupB.map(({
-          _id, country, flag, gamesWon, gamesLost, gamesWO, gamesLO, points,
-        }) => (
-          <TeamsCard
-            key={_id}
-            _id={_id}
-            country={country}
-            flag={flag}
-            gamesWon={gamesWon}
-            gamesLost={gamesLost}
-            gamesWO={gamesWO}
-            gamesLO={gamesLO}
-            points={points}
-            deletable={false}
-          />
-        ))}
       </div>
-    </div>
+    ) : (
+      <h2>No active season</h2>
+    )
   )
 }
 
