cb0ad7518bb9c678ba8242495477180ddb739d9d
Haralds Treilons <100586228+GARISoo@users.noreply.github.com>
2022-08-23 16:01:52 +0300

    update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,7 +11,7 @@ import './i18n'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<div>Loading..</div>}>
       <AuthContextProvider>
         <ModalContextProvider>
           <TotoContextProvider>
