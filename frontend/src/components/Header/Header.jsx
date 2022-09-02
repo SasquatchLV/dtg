33bf2894f5cb4bf7fc965218edcb05aadeaa99a5
Haralds Treilons <100586228+GARISoo@users.noreply.github.com>
2022-09-02 09:58:15 +0300

    Navigation update

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,19 +1,19 @@
-import { useNavigate } from 'react-router-dom'
 import { useState } from 'react'
+import { useNavigate, useLocation } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
 import { useLogout } from '../../hooks/useLogout'
 import { useAuthContext } from '../../hooks/useAuthContext'
 import styles from './Header.module.scss'
-import Navigation from '../Navigation/Navigation'
 import UserModal from '../UserModal/UserModal'
 
 const Header = () => {
+  const location = useLocation()
+  const [activeSection, setActiveSection] = useState(location.pathname)
   const [userModal, setUserModal] = useState(false)
   const { user } = useAuthContext()
   const { logout } = useLogout()
   const navigate = useNavigate()
   const { t, i18n } = useTranslation()
-
   const isAdmin = user?.roles?.includes(2000)
 
   const changeLanguage = (language) => {
@@ -24,57 +24,94 @@ const Header = () => {
 
   const toggleUserModal = () => setUserModal(!userModal)
 
+  const sections = [
+    {
+      id: 1,
+      title: t('header.matches'),
+      handleClick: () => {
+        setActiveSection('/matches')
+        navigate('/matches')
+      },
+      pathname: '/matches',
+    },
+    {
+      id: 2,
+      title: t('header.standings'),
+      handleClick: () => {
+        setActiveSection('/standings')
+        navigate('/standings')
+      },
+      pathname: '/standings',
+    },
+    {
+      id: 3,
+      title: t('header.leaderboard'),
+      handleClick: () => {
+        setActiveSection('/leaderboard')
+        navigate('/leaderboard')
+      },
+      pathname: '/leaderboard',
+    },
+  ]
+
   return (
-    user && (
-    <>
-      <header>
-        <div className={styles.languages}>
-          <button
-            className={styles.languageBtn}
-            onClick={() => changeLanguage('en')}
+    <div className={styles.container}>
+      <div className={styles.languages}>
+        <button
+          className={styles.languageBtn}
+          onClick={() => changeLanguage('en')}
+        >
+          ENG
+        </button>
+        <button
+          className={styles.languageBtn}
+          onClick={() => changeLanguage('lv')}
+        >
+          LAT
+        </button>
+      </div>
+      <div className={styles.navigation}>
+        {sections.map(({
+          id, title, pathname, handleClick,
+        }) => (
+          <span
+            className={pathname === activeSection ? styles.active : styles.section}
+            onClick={handleClick}
+            key={id}
           >
-            ENG
+            {title}
+          </span>
+        ))}
+      </div>
+      <div className={styles.info}>
+        {user && (
+        <div className={styles.actions}>
+          <button
+            className={styles.modalBtn}
+            onClick={toggleUserModal}
+          >
+            {user.email}
           </button>
+          {userModal && <UserModal handleToggle={toggleUserModal} />}
+          {isAdmin && (
           <button
-            className={styles.languageBtn}
-            onClick={() => changeLanguage('lv')}
+            className={styles.adminBtn}
+            onClick={handleAdmin}
           >
-            LAT
+            {t('header.admin')}
+          </button>
+          )}
+          <button
+            className={styles.logoutBtn}
+            onClick={logout}
+          >
+            {t('header.logout')}
           </button>
         </div>
-        <div className={styles.container}>
-          <div className={styles.info}>
-            {user && (
-            <div className={styles.actions}>
-              <button
-                className={styles.modalBtn}
-                onClick={toggleUserModal}
-              >
-                {user.email}
-              </button>
-              {userModal && <UserModal handleToggle={toggleUserModal} />}
-              {isAdmin && (
-              <button
-                className={styles.adminBtn}
-                onClick={handleAdmin}
-              >
-                {t('header.admin')}
-              </button>
-              )}
-              <button
-                className={styles.logoutBtn}
-                onClick={logout}
-              >
-                {t('header.logout')}
-              </button>
-            </div>
-            )}
-          </div>
-        </div>
-      </header>
-      <Navigation />
-    </>
-    ))
+        )}
+      </div>
+    </div>
+  )
 }
 
 export default Header
